412f40c4a96c094cbc76385266ae7e48d895b3b7
Rob Hoelz <rob@hoelz.ro>
2012-04-22 14:40:06 +0200

    Run REPL tests in a child process
    This is so the symbol table begins fresh with each test

--- t/lib/REPLTest.pm
+++ t/lib/REPLTest.pm
@@ -2,18 +2,29 @@ package REPLTest;
 
 use strict;
 use warnings;
+use autodie;
 use parent 'Exporter';
 
 use Devel::REPL;
 use Test::More;
+use Test::SharedFork;
 
 our @EXPORT = qw(test_repl);
 
 sub test_repl (&) {
     my ( $action ) = @_;
 
-    my $repl = Devel::REPL->new(term => {});
-    return $action->($repl);
+    my $pid = fork();
+
+    if($pid) {
+        waitpid $pid, 0;
+
+        return $? ? 0 : 1;
+    } else {
+        my $repl = Devel::REPL->new(term => {});
+        my $ok   = $action->($repl);
+        exit($ok ? 0 : 1);
+    }
 }
 
 1;
